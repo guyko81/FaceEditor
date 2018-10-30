39fd2f4f0bc0f3d1125795f7356d46cbf08988e3
Gábor Gulyás <guyko@Gabors-MacBook-Pro.local>
2018-10-30 13:55:59 +0100

    change to (w,h,c) and size=(218,178) and load_model->load hdf5

--- encoder.py
+++ encoder.py
@@ -1,7 +1,7 @@
 import sys, random
 import numpy as np
-from matplotlib import pyplot as plt
-from dutil import *
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import pydot
 
 SHIFT_AMOUNT = 9
@@ -16,37 +16,41 @@ NUM_RAND_FACES = BATCH_SIZE
 NUM_TEST_FACES = BATCH_SIZE
 
 def plotScores(scores, test_scores, fname, on_top=True):
-	plt.clf()
-	ax = plt.gca()
-	ax.yaxis.tick_right()
-	ax.yaxis.set_ticks_position('both')
-	ax.yaxis.grid(True)
-	plt.plot(scores)
-	plt.plot(test_scores)
-	plt.xlabel('Epoch')
-	plt.ylim([0.0, 0.01])
-	loc = ('upper right' if on_top else 'lower right')
-	plt.legend(['Train', 'Test'], loc=loc)
-	plt.draw()
-	plt.savefig(fname)
+    plt.clf()
+    ax = plt.gca()
+    ax.yaxis.tick_right()
+    ax.yaxis.set_ticks_position('both')
+    ax.yaxis.grid(True)
+    plt.plot(scores)
+    plt.plot(test_scores)
+    plt.xlabel('Epoch')
+    plt.ylim([0.0, 0.01])
+    loc = ('upper right' if on_top else 'lower right')
+    plt.legend(['Train', 'Test'], loc=loc)
+    plt.draw()
+    plt.savefig(fname)
 
 #Load data set
-print "Loading Data..."
-y_train = np.load('y_data.npy').astype(np.float32) / 255.0
+print ("Loading Data...")
+files = os.listdir("../input/img_align_celeba/img_align_celeba")
+images = []
+for t, file in enumerate(files[:2000]):
+    if t%100 == 0:
+        print(t)
+    images.append(mpimg.imread('../input/img_align_celeba/img_align_celeba/'+file))
+    
+y_train = np.array(images).astype(np.float32) / 255.0
 y_train = y_train[:y_train.shape[0] - y_train.shape[0] % BATCH_SIZE]
 x_train = np.expand_dims(np.arange(y_train.shape[0]), axis=1)
 num_samples = y_train.shape[0]
-print "Loaded " + str(num_samples) + " Samples."
+print ("Loaded " + str(num_samples) + " Samples.")
+
 
 ###################################
 #  Create Model
 ###################################
-print "Loading Keras..."
+print ("Loading Keras...")
 import os, math
-os.environ['THEANORC'] = "./gpu.theanorc"
-os.environ['KERAS_BACKEND'] = "theano"
-import theano
-print "Theano Version: " + theano.__version__
 
 from keras.initializers import RandomUniform
 from keras.layers import Input, Dense, Activation, Dropout, Flatten, Reshape, SpatialDropout2D
@@ -61,122 +65,127 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.regularizers import l1
 from keras.utils import plot_model
 from keras import backend as K
-from custom_layers import BinaryEncoder, PATCON
-K.set_image_data_format('channels_first')
 
 if CONTINUE_TRAIN:
-	print "Loading Model..."
-	model = load_model('Encoder.h5')
+    print ("Loading Model...")
+    model = load_model('Encoder.h5')
 else:
-	print "Building Model..."
-	model = Sequential()
+    print ("Building Model...")
+    model = Sequential()
 
-	model.add(Embedding(num_samples, PARAM_SIZE, input_length=1))
-	model.add(Flatten(name='pre_encoder'))
-	print model.output_shape
-	assert(model.output_shape == (None, PARAM_SIZE))
-	
-	model.add(Reshape((PARAM_SIZE, 1, 1), name='encoder'))
-	print model.output_shape
-	
-	model.add(Conv2DTranspose(256, (4, 1)))           #(4, 1)
-	model.add(Activation("relu"))
-	print model.output_shape
+    model.add(Embedding(num_samples, PARAM_SIZE, input_length=1))
+    model.add(Flatten(name='pre_encoder'))
+    print (model.output_shape)
+    assert(model.output_shape == (None, PARAM_SIZE))
+    
+    model.add(Reshape((1, 1, PARAM_SIZE), name='encoder'))
+    print (model.output_shape)
+    
+    model.add(Conv2DTranspose(256, (5, 4)))           #(5, 4)
+    model.add(Activation("relu"))
+    print (model.output_shape)
 
-	model.add(Conv2DTranspose(256, 4))                #(7, 4)
-	model.add(Activation("relu"))
-	print model.output_shape
-	
-	model.add(Conv2DTranspose(256, 4))                #(10, 7)
-	model.add(Activation("relu"))
-	print model.output_shape
-	
-	model.add(Conv2DTranspose(256, 4, strides=2))     #(22, 16)
-	model.add(Activation("relu"))
-	print model.output_shape
-	
-	model.add(Conv2DTranspose(128, 4, strides=2))     #(46, 34)
-	model.add(Activation("relu"))
-	print model.output_shape
-	
-	model.add(Conv2DTranspose(128, 4, strides=2))     #(94, 70)
-	model.add(Activation("relu"))
-	print model.output_shape
+    model.add(Conv2DTranspose(256, 4, strides=(1, 1)))     #(8, 7)
+    model.add(Activation("relu"))
+    print (model.output_shape)
+    
+    model.add(Conv2DTranspose(256, 4, strides=(1, 2)))      #(11, 16)
+    model.add(Activation("relu"))
+    print (model.output_shape)
+    
+    model.add(Conv2DTranspose(256, 4, strides=(2, 1)))     #(24, 19)
+    model.add(Activation("relu"))
+    print (model.output_shape)
+    
+    model.add(Conv2DTranspose(128, 4, strides=(2, 2)))     #(50, 40)
+    model.add(Activation("relu"))
+    print (model.output_shape)
+    
+    model.add(Conv2DTranspose(128, 4, strides=(1, 1)))     #(53, 43)
+    model.add(Activation("relu"))
+    print (model.output_shape)
+    
+    model.add(Conv2DTranspose(128, 4, strides=(2, 2)))     #(108, 88)
+    model.add(Activation("relu"))
+    print (model.output_shape)
+    
+    model.add(Conv2DTranspose(3, 4, strides=(2, 2)))      #(218, 178)
+    model.add(Activation("sigmoid"))
+    print (model.output_shape)
+    assert(model.output_shape[1:] == (218, 178, 3))
 
-	model.add(Conv2DTranspose(3, 6, strides=2))      #(192, 144)
-	model.add(Activation("sigmoid"))
-	print model.output_shape
-	assert(model.output_shape[1:] == (3, 192, 144))
-
-	model.compile(optimizer=Adam(lr=LR), loss='mse')
-	plot_model(model, to_file='model.png', show_shapes=True)
+    model.compile(optimizer=Adam(lr=LR), loss='mse')
+    plot_model(model, to_file='model.png', show_shapes=True)
 
 ###################################
 #  Encoder / Decoder
 ###################################
-print "Compiling SubModels..."
+print ("Compiling SubModels...")
 func = K.function([model.get_layer('encoder').input, K.learning_phase()],
-				  [model.layers[-1].output])
+                  [model.layers[-1].output])
 enc_model = Model(inputs=model.input,
                   outputs=model.get_layer('pre_encoder').output)
 
 rand_vecs = np.random.normal(0.0, 1.0, (NUM_RAND_FACES, PARAM_SIZE))
 
 def make_rand_faces(rand_vecs, iters):
-	x_enc = enc_model.predict(x_train, batch_size=BATCH_SIZE)
-	
-	x_mean = np.mean(x_enc, axis=0)
-	x_stds = np.std(x_enc, axis=0)
-	x_cov = np.cov((x_enc - x_mean).T)
-	e, v = np.linalg.eig(x_cov)
+    x_enc = enc_model.predict(x_train, batch_size=BATCH_SIZE)
+    
+    x_mean = np.mean(x_enc, axis=0)
+    x_stds = np.std(x_enc, axis=0)
+    x_cov = np.cov((x_enc - x_mean).T)
+    e, v = np.linalg.eig(x_cov)
 
-	np.save('means.npy', x_mean)
-	np.save('stds.npy', x_stds)
-	np.save('evals.npy', e)
-	np.save('evecs.npy', v)
-	
-	e_list = e.tolist()
-	e_list.sort(reverse=True)
-	plt.clf()
-	plt.bar(np.arange(e.shape[0]), e_list, align='center')
-	plt.draw()
-	plt.savefig('evals.png')
-	
-	x_vecs = x_mean + np.dot(v, (rand_vecs * e).T).T
-	y_faces = func([x_vecs, 0])[0]
-	for i in xrange(y_faces.shape[0]):
-		save_image(y_faces[i], 'rand' + str(i) + '.png')
-		if i < 5 and (iters % 10) == 0:
-			if not os.path.exists('morph' + str(i)):
-				os.makedirs('morph' + str(i))
-			save_image(y_faces[i], 'morph' + str(i) + '/img' + str(iters) + '.png')
-
+    np.save('means.npy', x_mean)
+    np.save('stds.npy', x_stds)
+    np.save('evals.npy', e)
+    np.save('evecs.npy', v)
+    
+    e_list = e.tolist()
+    e_list.sort(reverse=True)
+    plt.clf()
+    plt.bar(np.arange(e.shape[0]), e_list, align='center')
+    plt.draw()
+    plt.savefig('evals.png')
+    
+    x_vecs = x_mean + np.dot(v, (rand_vecs * e).T).T
+    y_faces = func([x_vecs, 0])[0]
+    for i in range(y_faces.shape[0]):
+        save_image(y_faces[i], 'rand' + str(i) + '.png')
+        if i < 5 and (iters % 10) == 0:
+            if not os.path.exists('morph' + str(i)):
+                os.makedirs('morph' + str(i))
+            save_image(y_faces[i], 'morph' + str(i) + '/img' + str(iters) + '.png')
+    imgplot = plt.imshow(y_faces[0])
+    plt.show()
 make_rand_faces(rand_vecs, 0)
 			
 ###################################
 #  Train
 ###################################
-print "Training..."
+print ("Training...")
 train_loss = []
 
-for iters in xrange(NUM_EPOCHS):
-	history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=1)
+for iters in range(NUM_EPOCHS):
+    history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=1)
 
-	loss = history.history['loss'][-1]
-	train_loss.append(loss)
-	print "Loss: " + str(loss)
+    loss = history.history['loss'][-1]
+    train_loss.append(loss)
+    print ("Loss: " + str(loss))
 
-	plotScores(train_loss, [], 'EncoderScores.png', True)
-	
-	if iters % 20 == 0:
-		model.save('Encoder.h5')
-		
-		y_faces = model.predict(x_train[:NUM_TEST_FACES], batch_size=BATCH_SIZE)
-		for i in xrange(y_faces.shape[0]):
-			save_image(y_faces[i], 'gt' + str(i) + '.png')
-		
-		make_rand_faces(rand_vecs, iters)
-		
-		print "Saved"
+    plotScores(train_loss, [], 'EncoderScores.png', True)
+    
+    if iters % 20 == 0:
+        model.save('Encoder.h5')
+        
+        y_faces = model.predict(x_train[:NUM_TEST_FACES], batch_size=BATCH_SIZE)
+        for i in range(y_faces.shape[0]):
+            save_image(y_faces[i], 'gt' + str(i) + '.png')
+        imgplot = plt.imshow(y_faces[0])
+        plt.show()
+            
+        make_rand_faces(rand_vecs, iters)
+        
+        print ("Saved")
 
-print "Done"
+print ("Done")
--- face_edit.py
+++ face_edit.py
@@ -7,13 +7,13 @@ from dutil import *
 
 #User constants
 device = "cpu"
-enc_fname = 'Encoder.h5'
+enc_fname = './model/Encoder.h5'
 background_color = (210, 210, 210)
 edge_color = (60, 60, 60)
 slider_color = (20, 20, 20)
 num_params = 80
-input_w = 144
-input_h = 192
+input_w = 178
+input_h = 218
 image_scale = 3
 image_padding = 10
 slider_w = 15
@@ -47,37 +47,128 @@ cur_face = np.zeros((3, input_h, input_w), dtype=np.uint8)
 rgb_array = np.zeros((input_h, input_w, 3), dtype=np.uint8)
 
 #Keras
-print "Loading Keras..."
+print ("Loading Keras...")
 import os
-os.environ['THEANORC'] = "./" + device + ".theanorc"
-os.environ['KERAS_BACKEND'] = "theano"
-import theano
-print "Theano Version: " + theano.__version__
-from keras.models import Sequential, load_model, model_from_json
-from keras.layers import Dense, Activation, Dropout, Flatten, Reshape
-from keras.layers.convolutional import Conv2D, Conv2DTranspose, ZeroPadding2D
+
+print ("Loading Encoder...")
+#enc_model = load_model(enc_fname)
+print ("Loading Keras...")
+import os, math
+
+from keras.initializers import RandomUniform
+from keras.layers import Input, Dense, Activation, Dropout, Flatten, Reshape, SpatialDropout2D
+from keras.layers.convolutional import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers.embeddings import Embedding
+from keras.layers.local import LocallyConnected2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.noise import GaussianNoise
-from keras.layers.local import LocallyConnected2D
+from keras.models import Model, Sequential, load_model
 from keras.optimizers import Adam, RMSprop, SGD
-from keras.regularizers import l2
-from keras.layers.advanced_activations import ELU
 from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l1
 from keras.utils import plot_model
 from keras import backend as K
-from custom_layers import BinaryEncoder
-K.set_image_data_format('channels_first')
 
-print "Loading Encoder..."
-enc_model = load_model(enc_fname)
-enc = K.function([enc_model.get_layer('encoder').input, K.learning_phase()],
-				 [enc_model.layers[-1].output])
+num_samples = 2000
+PARAM_SIZE = 80
+LR = 0.01
 
-print "Loading Statistics..."
-means = np.load('means.npy')
-stds  = np.load('stds.npy')
-evals = np.sqrt(np.load('evals.npy'))
-evecs = np.load('evecs.npy')
+print ("Building Model...")
+model = Sequential()
+
+model.add(Embedding(num_samples, PARAM_SIZE, input_length=1))
+model.add(Flatten(name='pre_encoder'))
+print (model.output_shape)
+assert(model.output_shape == (None, PARAM_SIZE))
+
+model.add(Reshape((1, 1, PARAM_SIZE), name='encoder'))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(256, (5, 4)))           #(5, 4)
+model.add(Activation("relu"))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(256, 4, strides=(1, 1)))     #(8, 7)
+model.add(Activation("relu"))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(256, 4, strides=(1, 2)))      #(11, 16)
+model.add(Activation("relu"))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(256, 4, strides=(2, 1)))     #(24, 19)
+model.add(Activation("relu"))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(128, 4, strides=(2, 2)))     #(50, 40)
+model.add(Activation("relu"))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(128, 4, strides=(1, 1)))     #(53, 43)
+model.add(Activation("relu"))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(128, 4, strides=(2, 2)))     #(108, 88)
+model.add(Activation("relu"))
+print (model.output_shape)
+
+model.add(Conv2DTranspose(3, 4, strides=(2, 2)))      #(218, 178)
+model.add(Activation("sigmoid"))
+print (model.output_shape)
+assert(model.output_shape[1:] == (218, 178, 3))
+
+model.compile(optimizer=Adam(lr=LR), loss='mse')
+
+
+import h5py
+data = h5py.File(enc_fname, 'r')
+group = data['model_weights']
+
+model.layers[0].set_weights([group['embedding_1']['embedding_1']['embeddings:0'].value])
+
+weight = [group['conv2d_transpose_1']['conv2d_transpose_1']['kernel:0'].value,
+          group['conv2d_transpose_1']['conv2d_transpose_1']['bias:0'].value]
+model.layers[3].set_weights(weight)
+
+weight = [group['conv2d_transpose_2']['conv2d_transpose_2']['kernel:0'].value,
+          group['conv2d_transpose_2']['conv2d_transpose_2']['bias:0'].value]
+model.layers[5].set_weights(weight)
+
+weight = [group['conv2d_transpose_3']['conv2d_transpose_3']['kernel:0'].value,
+          group['conv2d_transpose_3']['conv2d_transpose_3']['bias:0'].value]
+model.layers[7].set_weights(weight)
+
+weight = [group['conv2d_transpose_4']['conv2d_transpose_4']['kernel:0'].value,
+          group['conv2d_transpose_4']['conv2d_transpose_4']['bias:0'].value]
+model.layers[9].set_weights(weight)
+
+weight = [group['conv2d_transpose_5']['conv2d_transpose_5']['kernel:0'].value,
+          group['conv2d_transpose_5']['conv2d_transpose_5']['bias:0'].value]
+model.layers[11].set_weights(weight)
+
+weight = [group['conv2d_transpose_6']['conv2d_transpose_6']['kernel:0'].value,
+          group['conv2d_transpose_6']['conv2d_transpose_6']['bias:0'].value]
+model.layers[13].set_weights(weight)
+
+weight = [group['conv2d_transpose_7']['conv2d_transpose_7']['kernel:0'].value,
+          group['conv2d_transpose_7']['conv2d_transpose_7']['bias:0'].value]
+model.layers[15].set_weights(weight)
+
+weight = [group['conv2d_transpose_8']['conv2d_transpose_8']['kernel:0'].value,
+          group['conv2d_transpose_8']['conv2d_transpose_8']['bias:0'].value]
+model.layers[17].set_weights(weight)
+
+
+
+
+enc = K.function([model.get_layer('encoder').input, K.learning_phase()],
+				 [model.layers[-1].output])
+
+print ("Loading Statistics...")
+means = np.load('./model/means.npy')
+stds  = np.load('./model/stds.npy')
+evals = np.sqrt(np.load('./model/evals.npy'))
+evecs = np.load('./model/evecs.npy')
 
 sort_inds = np.argsort(-evals)
 evals = evals[sort_inds]
@@ -99,8 +190,8 @@ def update_mouse_click(mouse_pos):
 	y = (mouse_pos[1] - sliders_y)
 
 	if x >= 0 and y >= 0 and x < sliders_w and y < sliders_h:
-		slider_ix_w = x / slider_w
-		slider_ix_h = y / slider_h
+		slider_ix_w = int(x / slider_w)
+		slider_ix_h = int(y / slider_h)
 
 		cur_slider_ix = slider_ix_h * slider_cols + slider_ix_w
 		mouse_pressed = True
@@ -120,7 +211,7 @@ def update_mouse_move(mouse_pos):
 		needs_update = True
 
 def draw_sliders():
-	for i in xrange(num_params):
+	for i in range(num_params):
 		row = i / slider_cols
 		col = i % slider_cols
 		x = sliders_x + col * slider_w
@@ -132,16 +223,16 @@ def draw_sliders():
 		pygame.draw.line(screen, slider_color, (cx, cy_1), (cx, cy_2))
 		
 		py = y + int((cur_params[i] / 6.0 + 0.5) * (slider_h - slider_py*2)) + slider_py
-		pygame.draw.circle(screen, slider_color, (cx, py), slider_w/2 - slider_px)
-		
+		pygame.draw.circle(screen, slider_color, (int(cx), int(py)), int(slider_w/2) - slider_px)
+
 		cx_1 = x + slider_px
 		cx_2 = x + slider_w - slider_px
-		for j in xrange(7):
+		for j in range(7):
 			ly = y + slider_h/2 + (j-3)*(slider_h/7)
 			pygame.draw.line(screen, slider_color, (cx_1, ly), (cx_2, ly))
 
 def draw_face():
-	pygame.surfarray.blit_array(face_surface_mini, np.transpose(cur_face, (2, 1, 0)))
+	pygame.surfarray.blit_array(face_surface_mini, np.transpose(cur_face, (1, 0, 2)))
 	pygame.transform.scale(face_surface_mini, (drawing_w, drawing_h), face_surface)
 	pygame.draw.rect(screen, (0,0,0), (drawing_x, drawing_y, drawing_w, drawing_h), 1)
 	
